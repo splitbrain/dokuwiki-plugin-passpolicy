8e5a0f9a2c54da3dfd137df737341451f29e9978
Tim Weinhold <tim.weinhold@gmail.com>
2013-08-16 14:15:09 +0200

    translation update

---
+++ lang/de/settings.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Tim Weinhold <tim.weinhold@gmail.com>
+ */
+$lang['minlen']                = 'Mindestlänge für Userpasswörter';
+$lang['pools']                 = 'Benutze Zeichentypen für Passwörter';
+$lang['autotype']              = 'Wie sollen Passwörter generiert werden?';
+$lang['autotype_o_random']     = 'Zufallspasswort';
+$lang['autotype_o_pronouncable'] = 'Sprechendes Passwort';
+$lang['pools_lower']           = 'Kleinbuchstaben';
+$lang['pools_upper']           = 'Großbuchstaben';
+$lang['pools_numeric']         = 'Zahlen';
+$lang['pools_special']         = 'Sonderzeichen (z.B. !, $, #, %)';
