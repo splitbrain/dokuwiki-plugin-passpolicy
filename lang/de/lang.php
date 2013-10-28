eb841a228858f5f9c7ff3d9a201a51e8c38827ac
Cornelius Kölbel <corny@cornelinux.de>
2013-10-28 22:20:05 +0100

    translation update

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -4,11 +4,13 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Tim Weinhold <tim.weinhold@gmail.com>
+ * @author Cornelius Kölbel <corny@cornelinux.de>
  */
 $lang['badpasspolicy']         = 'Sie müssen ein stärkeres Passwort nutzen.';
 $lang['length']                = 'Ihr Passwort muss mindestens %d Zeichen lang sein.';
 $lang['pools']                 = 'Ihr Passwort benötigt mindestens %d Zeichen der folgenden Zeichentypen: %s.';
 $lang['user1']                 = 'Ihr Passwort darf nicht den Benutzernamen beinhalten.';
+$lang['user2']                 = 'Ihr Passwort darf nur %d oder weniger zusammenhängende Buchstaben enthalten, die auch in Ihrem Benutzernamen vorkommen.';
 $lang['js']['strength0']       = 'Sehr schwach';
 $lang['js']['strength1']       = 'Schwach';
 $lang['js']['strength2']       = 'Annehmbar';
