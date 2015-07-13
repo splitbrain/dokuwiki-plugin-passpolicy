1ffb56cf3b542af5625049a22f8ce73b2e947b52
Hideaki SAWADA <chuno@live.jp>
2015-07-13 05:40:05 +0200

    translation update

--- lang/ja/settings.php
+++ lang/ja/settings.php
@@ -4,11 +4,13 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Satoshi Sahara <sahara.satoshi@gmail.com>
+ * @author Hideaki SAWADA <chuno@live.jp>
  */
 $lang['minlen']                = '許容するパスワード長さの最小値';
 $lang['pools']                 = 'パスワードで使用可能な文字種';
 $lang['minpools']              = '最低限使用すべき文字種の数 （使用可能な文字種の数以下で設定）';
 $lang['user']                  = 'パスワード文字列内にユーザー名（の一部）が含まれているかどうかのチェック方法 （0: チェックしない, 1: 単純チェック, >2:許容するユーザー名部分の長さ）';
+$lang['nocommon']              = 'ありがちなパスワード 10,000 件一覧と照合する。';
 $lang['autotype']              = 'パスワードの生成方法';
 $lang['autobits']              = '生成するパスワードの情報ビット数の最小値 （ビット数が大きいほど強力なパスワード, 最小値: 24）';
 $lang['autotype_o_random']     = 'ランダムパスワード (random)';
