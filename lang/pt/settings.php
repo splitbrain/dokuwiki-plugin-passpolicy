6fcbf03b1dae435b7329e872d5537bb4ba6b4cbf
Alexandre Belchior Medeiros <alexbelchior@gmail.com>
2015-07-31 14:20:05 +0200

    translation update

---
+++ lang/pt/settings.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Alexandre Belchior Medeiros <alexbelchior@gmail.com>
+ */
+$lang['autotype']              = 'Como gerar senhas?';
+$lang['autotype_o_random']     = 'senha aleatória';
+$lang['autotype_o_pronouncable'] = 'senha pronunciável';
+$lang['pools_lower']           = 'letras em minúsculo';
+$lang['pools_upper']           = 'letras em maiúsculo';
+$lang['pools_numeric']         = 'números';
+$lang['pools_special']         = 'caracteres especiais(eg. !, $, #, %)';
