edb6140eac3e233f74a5f5a64ff474c587ed203a
Mark C. Prins <mprins@users.sf.net>
2015-08-10 13:15:37 +0200

    translation update

--- lang/nl/settings.php
+++ lang/nl/settings.php
@@ -4,11 +4,13 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Gerrit Uitslag <klapinklapin@gmail.com>
+ * @author Mark C. Prins <mprins@users.sf.net>
  */
 $lang['minlen']                = 'Minimale lengte voor gebruikerswachtwoorden';
 $lang['pools']                 = 'Karaktertypes te gebruiken in wachtwoorden';
 $lang['minpools']              = 'Minimale aantal verschillende karaktertypes dat gebruikt moeten worden in wachtwoorden. Mag niet hoger zijn dan het aantal hierboven geselecteerde types.';
 $lang['user']                  = 'Controleer of het wachtwoord overeenkomt met de gebruikernaam. 0 is uitschakelen. 1 voor exacte overeenkomst. Geef een ander nummer voor het aantal opeenvolgende karakters dat in beide, wachtwoord én gebruikernaam, mag voorkomen.';
+$lang['nocommon']              = 'Controleer het wachtewoord tegen de lijst met 10000 meest gebruikte wachtwoorden.';
 $lang['autotype']              = 'Hoe wachtwoorden genereren?';
 $lang['autobits']              = 'Minimale aantal bits van informatie om een wachtwoord te genereren. Hoe hoger, hoe moeilijker om te onthouden. Minimum: 24.';
 $lang['autotype_o_random']     = 'Willekeurig wachtwoord';
