4158b2ca0dadfc1acb7217615c82c9bbace93728
liz <marliza@web.de>
2016-01-21 11:30:24 +0100

    translation update

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -5,16 +5,18 @@
  * 
  * @author Tim Weinhold <tim.weinhold@gmail.com>
  * @author Cornelius Kölbel <corny@cornelinux.de>
+ * @author liz <marliza@web.de>
  */
-$lang['minlen']                = 'Mindestlänge für Userpasswörter';
-$lang['pools']                 = 'Benutze Zeichentypen für Passwörter';
-$lang['minpools']              = 'Die minimale Anzahl an unterschiedlichen Zeichentypen, die in einem Passwort verwendet werden muss. Dies darf nicht größer als die Anzahl der oben ausgewählten Typen sein.';
-$lang['user']                  = 'Überprüfen, ob das Passwort zum Benutzernamen passt. 0 schaltet die Prüfung ab. 1 entspricht der exakten Übereinstimmung. Jeder andere Zahl entspricht der Anzahl der zusammenhängenden Zeichen, die im Benutzernamen und im Passwort gleich vorkommen dürfen.';
+$lang['minlen']                = 'Mindestlänge für Benutzerpasswörter';
+$lang['pools']                 = 'Für Passwörter geeignete Schriftzeichen';
+$lang['minpools']              = 'Minimale Anzahl der unterschiedlichen Schriftzeichen, die in einem Passwort verwendet werden muss. Darf nicht größer als die Anzahl der oben ausgewählten Zeichen sein.';
+$lang['user']                  = 'Überprüfen, ob das Passwort zum Benutzernamen passt. 0 schaltet die Prüfung ab. 1 entspricht der exakten Übereinstimmung. Jede andere Zahl entspricht der Anzahl der zusammenhängenden Zeichen, die sowohl im Benutzernamen als auch im Passwort vorkommen dürfen.';
+$lang['nocommon']              = 'Vergleiche Passwort mit einer Liste der 10.000 geläufigsten Passwörter.';
 $lang['autotype']              = 'Wie sollen Passwörter generiert werden?';
 $lang['autobits']              = 'Minimale Anzahl an Informationsbits zur Generierung eines Passwortes. Je höher die Zahl ist, desto sicherer ist das Passwort aber auch um so schwerer zu merken. Minimum: 24';
 $lang['autotype_o_random']     = 'Zufallspasswort';
-$lang['autotype_o_pronouncable'] = 'Sprechendes Passwort';
-$lang['autotype_o_phrase']     = 'Passphrase aus mehreren Wörtern';
+$lang['autotype_o_pronouncable'] = 'Aussprechbares Passwort';
+$lang['autotype_o_phrase']     = 'Passwortsatz aus mehreren Wörtern';
 $lang['pools_lower']           = 'Kleinbuchstaben';
 $lang['pools_upper']           = 'Großbuchstaben';
 $lang['pools_numeric']         = 'Zahlen';
