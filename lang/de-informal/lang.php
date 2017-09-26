f23d7c31704271cbd40ff950039449a9bcc55212
F. Mueller-Donath <j.felix@mueller-donath.de>
2017-09-26 16:25:29 +0200

    translation update

--- lang/de-informal/lang.php
+++ lang/de-informal/lang.php
@@ -4,13 +4,14 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
  * @author Felix Müller-Donath <j.felix@mueller-donath.de>
+ * @author F. Mueller-Donath <j.felix@mueller-donath.de>
  */
-$lang['badpasspolicy']         = 'Du müssen ein stärkeres Passwort verwenden.';
+$lang['badpasspolicy']         = 'Du musst ein stärkeres Passwort verwenden.';
 $lang['length']                = 'Dein Passwort muss mindestens %d Zeichen lang sein.';
 $lang['pools']                 = 'Dein Passwort benötigt mindestens %d Zeichen der folgenden Zeichentypen: %s.';
-$lang['user1']                 = 'Dein Passwort darf nicht den Benutzernamen entnhalten.';
+$lang['user1']                 = 'Dein Passwort darf nicht den Benutzernamen enthalten.';
 $lang['user2']                 = 'Dein Passwort darf nur %d oder weniger zusammenhängende Buchstaben, die auch in deinem Benutzernamen vorkommen, enthalten.';
-$lang['nocommon']              = 'Dein Passwort ist nicht unter den 10.000 meist genutzten Passworten.';
+$lang['nocommon']              = 'Dein Passwort darf nicht in der Liste der 10.000 beliebtesten Passwörter enthalten sein.';
 $lang['js']['strength0']       = 'sehr schwach';
 $lang['js']['strength1']       = 'schwach';
 $lang['js']['strength2']       = 'annehmbar';
