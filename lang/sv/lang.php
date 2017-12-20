5b9465d692207f105500ef7a664bbc6883ed3d9b
Tor Härnqvist <tor@harnqvist.se>
2017-12-20 20:35:39 +0100

    translation update

---
+++ lang/sv/lang.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Tor Härnqvist <tor@harnqvist.se>
+ */
+$lang['badpasspolicy']         = 'Du måste välja ett starkare lösenord.';
+$lang['length']                = 'Ditt lösenord måste vara minst %d tecken långt.';
+$lang['pools']                 = 'Ditt lösenord måste använda tecken från minst %d av följande varianter: %s.';
+$lang['user1']                 = 'Ditt lösenord får inte innehålla ditt användarnamn.';
+$lang['user2']                 = 'Ditt lösenord får bara innehålla %d eller färre konsekutiva tecken som ingår i ditt lösenord.';
+$lang['nocommon']              = 'Ditt lösenord från inte vara ett av de 10 000 vanligaste lösenorden.';
+$lang['js']['strength0']       = 'väldigt svagt';
+$lang['js']['strength1']       = 'svagt';
+$lang['js']['strength2']       = 'hyfsat';
+$lang['js']['strength3']       = 'starkt';
