bab37ed442b39681937b75aa7b96a4ded9178c8b
Alexandre Belchior <alexbelchior@gmail.com>
2018-07-05 16:05:18 +0200

    translation update

---
+++ lang/pt-br/lang.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Alexandre Belchior <alexbelchior@gmail.com>
+ */
+$lang['badpasspolicy']         = 'Você tem que usar uma senha mais forte.';
+$lang['length']                = 'Sua senha precisa ter pelo menos %d caracteres.';
+$lang['pools']                 = 'Sua senha precisa usar caracteres de pelo menos %d dos seguintes tipos: %s.';
+$lang['user1']                 = 'Sua senha não pode conter seu nome de usuário.';
+$lang['user2']                 = 'Sua senha pode usar apenas %d ou menos caracteres consecutivos que aparecem em seu nome de usuário.';
+$lang['nocommon']              = 'Sua senha pode não ser uma das 10.000 senhas mais usadas.';
+$lang['js']['strength0']       = 'muito fraca';
+$lang['js']['strength1']       = 'fraca';
+$lang['js']['strength2']       = 'decente';
+$lang['js']['strength3']       = 'forte';
+$lang['js']['nopolicy']        = ', política não cumprida';
