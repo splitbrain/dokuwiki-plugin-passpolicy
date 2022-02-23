b7f5c4d32276eb5c366c2e3e59f1778c6fcd7462
Krabyyx <ahsystems@outlook.fr>
2022-02-23 15:20:42 +0100

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -3,15 +3,17 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Krabyyx <ahsystems@outlook.fr>
  * @author Nicolas Friedli <nicolas@theologique.ch>
  */
-$lang['badpasspolicy'] = 'Vous devez utiliser un mot de passe plus fort.';
-$lang['length'] = 'Votre mot de passe doit comporter au moins %d caractères.';
-$lang['pools'] = 'Votre mot de passe doit comporter au moins %d caractères des types suivants: %s.';
-$lang['user1'] = 'Votre mot de passe ne doit pas contenir votre identifiant utilisateur.';
-$lang['user2'] = 'Votre mot de passe ne peut contenir que %d ou moins de caractères consécutifs de votre identifiant utilisateur.';
-$lang['nocommon'] = 'Votre mot de passe ne devrait pas appartenir aux 10000 mots de passe les plus courants.';
-$lang['js']['strength0'] = 'très faible';
-$lang['js']['strength1'] = 'faible';
-$lang['js']['strength2'] = 'acceptable';
-$lang['js']['strength3'] = 'fort';
+$lang['badpasspolicy']         = 'Vous devez utiliser un mot de passe plus fort.';
+$lang['length']                = 'Votre mot de passe doit comporter au moins %d caractères.';
+$lang['pools']                 = 'Votre mot de passe doit comporter au moins %d caractères des types suivants: %s.';
+$lang['user1']                 = 'Votre mot de passe ne doit pas contenir votre identifiant utilisateur.';
+$lang['user2']                 = 'Votre mot de passe ne peut contenir que %d ou moins de caractères consécutifs de votre identifiant utilisateur.';
+$lang['nocommon']              = 'Votre mot de passe ne devrait pas appartenir aux 10000 mots de passe les plus courants.';
+$lang['resendpwd']             = 'Un mail de confirmation a été envoyé à l\'utilisateur donné, si le compte existe.';
+$lang['js']['strength0']       = 'très faible';
+$lang['js']['strength1']       = 'faible';
+$lang['js']['strength2']       = 'acceptable';
+$lang['js']['strength3']       = 'fort';
