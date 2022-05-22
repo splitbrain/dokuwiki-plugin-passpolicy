b88b405c8f2867316313e0ab89b4056cbaa2127e
铁桶 <2335652849@qq.com>
2022-05-22 04:00:25 +0200

    translation update

---
+++ lang/zh-tw/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author 铁桶 <2335652849@qq.com>
+ */
+$lang['autotype']              = '如何生成密码？';
+$lang['autotype_o_random']     = '随机密码';
+$lang['pools_numeric']         = '数字';
+$lang['pools_special']         = '特殊字符（例如!、$、#、%）';
