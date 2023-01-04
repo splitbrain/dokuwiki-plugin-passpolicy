a10e734c29fbc6d1aa8d56af3430f23c5ea3a21f
Hella <hella.breitkopf@gmail.com>
2023-01-04 13:05:12 +0100

    translation update

--- lang/de-informal/lang.php
+++ lang/de-informal/lang.php
@@ -3,16 +3,19 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Hella <hella.breitkopf@gmail.com>
  * @author Felix Müller-Donath <j.felix@mueller-donath.de>
- * @author F. Mueller-Donath <j.felix@mueller-donath.de>
  */
-$lang['badpasspolicy'] = 'Du musst ein stärkeres Passwort verwenden.';
-$lang['length'] = 'Dein Passwort muss mindestens %d Zeichen lang sein.';
-$lang['pools'] = 'Dein Passwort benötigt mindestens %d Zeichen der folgenden Zeichentypen: %s.';
-$lang['user1'] = 'Dein Passwort darf nicht den Benutzernamen enthalten.';
-$lang['user2'] = 'Dein Passwort darf nur %d oder weniger zusammenhängende Buchstaben, die auch in deinem Benutzernamen vorkommen, enthalten.';
-$lang['nocommon'] = 'Dein Passwort darf nicht in der Liste der 10.000 beliebtesten Passwörter enthalten sein.';
-$lang['js']['strength0'] = 'sehr schwach';
-$lang['js']['strength1'] = 'schwach';
-$lang['js']['strength2'] = 'annehmbar';
-$lang['js']['strength3'] = 'stark';
+$lang['badpasspolicy']         = 'Sie müssen ein stärkeres Passwort verwenden.';
+$lang['length']                = 'Ihr Passwort muss mindestens %d Zeichen lang sein.';
+$lang['pools']                 = 'Ihr Passwort benötigt mindestens %d Zeichen der folgenden Zeichentypen: %s.';
+$lang['user1']                 = 'Ihr Passwort darf nicht den Benutzernamen enthalten.';
+$lang['user2']                 = 'Ihr Passwort darf nur %d oder weniger zusammenhängende Buchstaben, die auch in Ihrem Benutzernamen vorkommen, enthalten.';
+$lang['nocommon']              = 'Ihr Passwort darf nicht in der Liste der 10.000 beliebtesten Passwörter enthalten sein.';
+$lang['noleaked']              = 'Ihr Passwort wurde vermutlich in keinem bekannten Passwortleck verwendet.';
+$lang['resendpwd']             = 'Eine Bestätigungsmail wurde an den Benutzer geschickt (falls der Account existiert).';
+$lang['js']['strength0']       = 'sehr schwach';
+$lang['js']['strength1']       = 'schwach';
+$lang['js']['strength2']       = 'annehmbar';
+$lang['js']['strength3']       = 'stark';
+$lang['js']['nopolicy']        = ', Passwort-Richtlinie nicht erfüllt';
